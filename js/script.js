ac13914797751f63e6f0ccd8342d9a1693fbc5fc
Giovanna Capitani <133259476+Gi-Capitani@users.noreply.github.com>
2023-07-07 18:27:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,15 +14,28 @@
     -passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
 */
 
-  // Obtém todos os botões com a classe "botoes-carrossel"
+// Obtém todos os botões com a classe "botoes-carrossel"
 const botoes = document.querySelectorAll('.botoes-carrossel');
 
-  // Adiciona um ouvinte de eventos para cada botão
+// Adiciona um ouvinte de eventos para cada botão
 botoes.forEach(botao => {
-    botao.addEventListener('click', exibirImagem);
+  botao.addEventListener('click', exibirImagem);
 });
 
 function exibirImagem(event) {
-    const target = event.target;
-    const imagemId = target.getAttribute('data-target');
-    const imagem = document.getElementById(imagemId);
+  const target = event.target;
+  const imagemId = target.getAttribute('data-target');
+  const imagem = document.getElementById(imagemId);
+
+  // Esconde todas as imagens
+  const imagens = document.querySelectorAll('.carrossel');
+  imagens.forEach(img => {
+    img.style.display = 'none';
+  });
+
+  // Exibe a imagem correspondente ao botão clicado
+  imagem.style.display = 'block';
+}
+
+
+ 
